0cbcc94e1d2c080a2f09031436a9a83e4acc45c2
Beybut Abdulragimov <31037730+BeyCoder@users.noreply.github.com>
2020-02-10 00:06:56 +0400

    Result parser

--- src/BeyCoder/Database/DatabaseResult.php
+++ src/BeyCoder/Database/DatabaseResult.php
@@ -2,9 +2,62 @@
 
 namespace BeyCoder;
 
+use Exception;
+
 class DatabaseResult{
-    
-    public function __construct()
+
+    /**
+     * @var string $data;
+     */
+    private $data;
+
+    /**
+     * @var array $deserializeData
+     */
+    private $deserializeData;
+
+    /**
+     * DatabaseResult constructor.
+     * @param string $jsonData
+     *
+     * @throws Exception
+     */
+    public function __construct(string $jsonData)
     {
+        $this->setData($jsonData);
+    }
+
+
+
+    /**
+     * @param string $data
+     *
+     * @throws Exception
+     */
+    public function setData(string $data)
+    {
+        $result = json_decode($data);
+        if($result == null || $result == false)
+        {
+            throw new Exception(json_last_error_msg());
+        }
+
+        $this->data = $data;
+    }
+
+    /**
+     * @return array
+     */
+    public function getData(): array
+    {
+        return $this->deserializeData;
+    }
+
+    /**
+     * @return bool
+     */
+    public function haveError() : bool
+    {
+        return empty($this->deserializeData["error"]);
     }
 }
