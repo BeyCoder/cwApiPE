b32f9f24ba4fa3e68306fa7edcd93f248f5b8b74
Beybut Abdulragimov <beybut2013@gmail.com>
2020-02-11 15:41:34 +0400

    hostApi update

---
+++ hostApi/Lang.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BeyCoder\HostAPI;
+
+include_once "db.php";
+
+use R;
+
+class Lang
+{
+
+    /**
+     * @return array|false|string
+     */
+    public static function getAllData()
+    {
+        $lang= R::findAll("lang");
+
+        $result = array();
+
+        if(!$lang){
+            foreach ($lang as $data){
+                $data[$data->key] = ["ru" => $data->ru_data];
+                $result["lang"][] = $data;
+            }
+        }else{
+            $result["error"] = ["code" => 801, "error_message" => "No lang keys!"];
+        }
+
+        $result = json_encode($result);
+        return $result;
+    }
+}
